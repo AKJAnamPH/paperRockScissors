ef6280a3b279319f45d30f26b7baf42094b55e78
AKJAnamPH <140148385+AKJAnamPH@users.noreply.github.com>
2023-08-18 14:30:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
-function add7(num){
-    console.log(num+7); 
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtn = document.querySelectorAll(".choiceBtn");
+
+let player;
+let computer;
+let result;
+
+choiceBtn.forEach(button => button.addEventListener("click", () => {
+
+
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Player: ${computer}`;
+    resultText.textContent = checkWinner();
+}));
+
+function computerTurn() {
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch (randNum) {
+        case 1:
+            computer = "ROCK";
+            break;
+        case 2:
+            computer = "PAPER";
+            break;
+        case 3:
+            computer = "SCISSORS";
+            break;
+    }
 }
-add7(8);
+
+function checkWinner() {
+    if(player == computer){
+        return "DRAW!";
+    }
+    else if (computer == "ROCK"){
+        return(player == "PAPER") ? "WIN!" : "LOSE!"
+    }
+    else if (computer == "PAPER"){
+        return(player == "SCISSORS") ? "WIN!" : "LOSE!"
+    }
+    else if (computer == "SCISSORS"){
+        return(player == "ROCK") ? "WIN!" : "LOSE!"
+    }   
+}
